47e78a090e91ee1023852a78e38ea4ac7459089a
wesleip <thesnp@hotmail.com>
2021-09-23 15:15:14 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="assets/avatar.png" type="image/x-icon" />
+    <link rel="stylesheet" href="styles.css">
+    <title>Meus Links</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+        crossorigin="anonymous" />
+</head>
+
+<body>
+    <div class="container">
+        <div class="tree">
+            <div class="header"><img src="assets/avatar.svg" class="profile-pic" /></div>
+            <div class="username"><a href="#"
+                    class="card-link active text-white" role="button" aria-pressed="true" target="_blank">
+                    @instagram</a></div>
+            <div class="links">
+                <a href="#" target="_blank" class="btn face btn-lg btn-block"
+                    style="border-radius: 25px;">
+                    <i class="fab fa-facebook-f mr-2"></i>Facebook</a>
+                <a href="#" target="_blank"
+                    class="btn yout btn-lg btn-block" style="border-radius: 25px;">
+                    <i class="fab fa-youtube mr-2"></i>Youtube</a>
+                <a href="#" target="_blank" class="btn telegram btn-lg btn-block"
+                    style="border-radius: 25px;">
+                    <i class="fab fa-telegram-plane mr-2"></i>Telegram</a>
+                <a href="#" target="_blank"
+                    class="btn linkedin btn-lg btn-block" style="border-radius: 25px;">
+                    <i class="fab fa-linkedin mr-2"></i>Linkedin</a>
+					<a href="#" target="_blank"
+                    class="btn github btn-lg btn-block" style="border-radius: 25px;">
+                    <i class="fab fa-github mr-2"></i>Github</a>
+					<a href="#" target="_blank"
+                    class="btn spotify btn-lg btn-block" style="border-radius: 25px;">
+                    <i class="fab fa-spotify mr-2"></i>Spotify</a>
+					<a href="#" target="_blank"
+                    class="btn discord btn-lg btn-block" style="border-radius: 25px;">
+                    <i class="fab fa-discord mr-2"></i>Discord</a>
+            </div>
+            <div class="footer letter">
+                &copy; <a href="#" target="_blank"
+                    class="card-link letter text-white"></a> - Todos os direitos reservados.
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
